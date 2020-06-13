f0bd2c18376079b86db5b1a6c29b5b7c7ba9decb
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-13 13:50:52 +0100

    Added authentication backends settings from allauth docs on settings.py

--- mukta/settings.py
+++ mukta/settings.py
@@ -59,13 +59,22 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 WSGI_APPLICATION = 'mukta.wsgi.application'
 
