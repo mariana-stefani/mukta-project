bbb7fc730f7fd8042ab554bfe392197fac3d6eec
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-13 16:21:20 +0100

    Added settings to access base templates on settings.py

--- mukta/settings.py
+++ mukta/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'mukta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates', 'layout'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
