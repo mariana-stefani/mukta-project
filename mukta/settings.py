eee54f4183c3bfe5a637d1fa8a04fff2f5941f35
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-13 14:01:08 +0100

    Added allauth apps to installed apps on settings.py

--- mukta/settings.py
+++ mukta/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +78,8 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+SITE_ID = 1
 
 
 WSGI_APPLICATION = 'mukta.wsgi.application'
