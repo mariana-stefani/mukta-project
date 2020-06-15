486fc777912f67f00b4a12f9c90def277ff81550
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-15 17:04:18 +0100

    Fixed typo on products/models.py

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(nax-length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
