d40e55f0eb9217d84f1ac9159e95d8bde1b03f92
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-15 17:38:12 +0100

    Added ProductAdmin class on products/admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
 admin.site.register(Product)
 admin.site.register(Category)
