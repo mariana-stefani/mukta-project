c7c80790a06947fdca40d4936836090e69bdcc44
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-06-16 18:33:20 +0100

    Updated all_products view to query the products name and description on products/views.py

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 # Create your views here.
 
@@ -7,6 +8,7 @@ def all_products(request):
     """ A view to display all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
 
     if request.GET:
         if 'q' in request.GET:
@@ -14,9 +16,13 @@ def all_products(request):
             if not query:
                 messages.error(request, "Please enter a search criteria.")
                 return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
