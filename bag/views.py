94e255a42399ea14b85699678141f150402eda2c
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-07-11 12:03:05 +0100

    Fixed typo on bag/views.py

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    option = None
+    colour = None
     if 'product_colour' in request.POST:
         colour = request.POST['colour']
     bag = request.session.get('bag', {})
