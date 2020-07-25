2caf1e933a9ac04ae5314973dcb5d1d860551075
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-07-25 17:22:32 +0100

    Fixed identation on profiles/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -9,13 +9,13 @@ def profile(request):
     """ Display the users's profile """
     profile = get_object_or_404(UserProfile, user=request.user)
     if request.method == 'POST':
-    form = UserProfileForm(request.POST, instance=profile)
-    if form.is_valid():
-        form.save()
-        messages.success(request, 'Profile updated successfully')
-    else:
-        messages.error(
-            request, 'Update failed. Please ensure the form is valid.')
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
