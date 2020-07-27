b63171fa83e4498832b1a1acdad7c9c7b92effbc
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-07-27 20:04:48 +0100

    Added reviews view in reviews/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def reviews(request):
+    """ A view to return the reviews page """
+    
+    return render(request, 'reviews/reviews.html')
