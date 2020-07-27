3dd5fdfa438d431f65bb6fd507eaa063c9603d33
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-07-27 21:39:27 +0100

    Added data for test purpose on reviews/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'username': 'Mari',
+        'title': 'First Review',
+        'content': 'Great service',
+        'date_posted': 'July 27, 2020'
+    },
+    {
+        'username': 'Test',
+        'title': 'Second Review',
+        'content': 'Loved the service',
+        'date_posted': 'July 24, 2020'
+    }
+]
+
+
 def reviews(request):
     """ A view to return the reviews page """
-    
-    return render(request, 'reviews/reviews.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'reviews/reviews.html', context)
