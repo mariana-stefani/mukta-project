387633af953a9a2092c17ee9d5e9bf8ed75cf06f
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-07-28 21:15:33 +0100

    Added review model and fields on reviews/models.py

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friendly_name = models.CharField(max_length=50, null=False, blank=False, default='')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
