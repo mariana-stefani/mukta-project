c4a3d4d11b67655fc957e924d9739576809aee72
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 15:19:59 +0100

    Added review-update url path on reviews/urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReviewListView, ReviewCreateView
+from .views import ReviewListView, ReviewCreateView, ReviewUpdateView
 from . import views
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='reviews'),
     path('new/', ReviewCreateView.as_view(), name='review-create'),
+    path('<int:pk>/update/', ReviewUpdateView.as_view(), name='review-update'),
 ]
