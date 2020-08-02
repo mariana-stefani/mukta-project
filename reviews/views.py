a1ab85b7122e8544b876555e5b8492b29415b4f7
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 14:57:54 +0100

    Created class view for ReviewCreateView on reviews/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Review
 
 # Create your views here.
+
 
 class ReviewListView(ListView):
     model = Review
     template_name = 'reviews/reviews.html'
     context_object_name = 'reviews'
     ordering = ['-date_posted']
+
+
+class ReviewCreateView(CreateView):
+    model = Review
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+        return ('reviews')
