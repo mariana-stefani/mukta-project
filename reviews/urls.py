97f1d4566ec02aeacb59a326920dcbc0bed31dda
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 15:35:59 +0100

    Added url for review-detail on reviews/urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ReviewListView, ReviewCreateView, ReviewUpdateView
+from .views import ReviewListView, ReviewCreateView, ReviewDetailView, ReviewUpdateView
 from . import views
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='reviews'),
     path('new/', ReviewCreateView.as_view(), name='review-create'),
+    path('<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
     path('<int:pk>/update/', ReviewUpdateView.as_view(), name='review-update'),
 ]
