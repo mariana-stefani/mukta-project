7adcf630afd124a8e86adb81a32a1cae4330c0a4
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 15:18:41 +0100

    Added class view ReviewUpdateView on reviews/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Review
 
 # Create your views here.
@@ -12,7 +13,18 @@ class ReviewListView(ListView):
     ordering = ['-date_posted']
 
 
-class ReviewCreateView(CreateView):
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+        return ('reviews')
+
+
+class ReviewUpdateView(LoginRequiredMixin, UpdateView):
     model = Review
     fields = ['title', 'content']
 
