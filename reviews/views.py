80d59cbcd80afdcae6f84d48ef773de0b1ebbdc2
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 13:52:27 +0100

    Removed reviews view from reviews/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -4,14 +4,6 @@ from .models import Review
 
 # Create your views here.
 
-
-def reviews(request):
-    """ A view to return the reviews page """
-    context = {
-        'reviews': Review.objects.all()
-    }
-    return render(request, 'reviews/reviews.html', context)
-
 class ReviewListView(ListView):
     model = Review
     template_name = 'reviews/reviews.html'
