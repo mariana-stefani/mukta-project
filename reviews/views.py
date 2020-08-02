5932368edc05a57d7450c9cfd52c3a8e693aa782
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 13:50:37 +0100

    Added ReviewListView class on reviews/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Review
 
 # Create your views here.
@@ -10,3 +11,9 @@ def reviews(request):
         'reviews': Review.objects.all()
     }
     return render(request, 'reviews/reviews.html', context)
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = 'reviews/reviews.html'
+    context_object_name = 'reviews'
+    ordering = ['-date_posted']
