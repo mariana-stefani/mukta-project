2d9f0d9c03f072f5dc35721855cc664ed9814133
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-02 14:56:32 +0100

    Added url path for review-create on reviews/urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ReviewListView
+from .views import ReviewListView, ReviewCreateView
 from . import views
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='reviews'),
+    path('new/', ReviewCreateView.as_view(), name='review-create'),
 ]
