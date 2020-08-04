676cd66df33445a879724f38c1ab2ce4b66a757d
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-04 18:14:24 +0100

    Added url path for review-delete on reviews/urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReviewListView, ReviewCreateView, ReviewDetailView, ReviewUpdateView
+from .views import ReviewListView, ReviewCreateView, ReviewDetailView, ReviewUpdateView, ReviewDeleteView
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('new/', ReviewCreateView.as_view(), name='review-create'),
     path('<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
     path('<int:pk>/update/', ReviewUpdateView.as_view(), name='review-update'),
+    path('<int:pk>/delete/', ReviewDeleteView.as_view(), name='review-delete'),
 ]
