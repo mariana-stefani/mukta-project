b640602ad106dad6ce28485168e49df6df78302c
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-08 14:44:21 +0100

    Applied migrations for workshops app

--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime, date
 
 # Create your models here.
+
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    location = models.CharField(max_length=100)
+    time = models.CharField(max_length=10)
+    instructor = models.CharField(max_length=100)
+    content = models.TextField()
+    images = models.URLField(max_length=1024, null=True, blank=True)
