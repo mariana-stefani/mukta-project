4e198e6993c9665466832b2fc32d15b13a1f9587
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-08 19:17:11 +0100

    Added WorkshopDetailView and WorkshopCreateView on workshops/views.py

--- workshops/views.py
+++ workshops/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Workshop
 
 # Create your views here.
+
 
 class WorkshopListView(ListView):
     model = Workshop
     template_name = 'workshops/workshops.html'
     context_object_name = 'workshops'
+
+
+class WorkshopDetailView(DetailView):
+    model = Workshop
+    template_name = 'workshops/workshop-detail.html'
+    context_object_name = 'workshop-detail'
+
+
+class WorkshopCreateView(CreateView):
+    model = Workshop
+    template_name = 'workshops/workshop-form.html'
+    context_object_name = 'workshop-create'
+    fields = ['title', 'date', 'location',
+              'time', 'instructor', 'content', 'images']
+    success_url = '/workshops'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
