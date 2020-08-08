4b460c774b2823385f369f1800cc46f32341d35c
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-08 15:32:57 +0100

    Removed dummy data from workshops/views.py

--- workshops/views.py
+++ workshops/views.py
@@ -3,29 +3,6 @@ from .models import Workshop
 
 # Create your views here.
 
-events = [
-    {
-        'title': 'Meditation Workshop',
-        'date': '26th September 2020',
-        'location': 'Hyde Park - London',
-        'time': '16:00',
-        'instructor': 'Mariana Stefani',
-        'content': 'Come and join us for a meditation workshop',
-        'images': 'https://cdn.pixabay.com/photo/2016/11/02/11/08/meditation-1791113_1280.jpg',
-
-    },
-    {
-        'title': 'Mantras Workshop',
-        'date': '12th October 2020',
-        'location': 'Regent\'s Park - London',
-        'time': '14:00',
-        'instructor': 'Mariana Stefani',
-        'content': 'Come and join us for an afternoon singing mantras',
-        'images': 'https://cdn.pixabay.com/photo/2020/05/08/13/33/lotus-5145730_1280.jpg',
-
-    }
-]
-
 
 def workshops(request):
     context = {
