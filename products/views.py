3665ddbda2ba04ffb317b8eaf4823bc0ca1c4ef5
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-12 19:28:48 +0100

    Updated product_detail.html to product-detail.html

--- products/views.py
+++ products/views.py
@@ -65,4 +65,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/product-detail.html', context)
