93fa0e3d2f94f3c1dfd437e50757b34ce2e57a0d
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-12 19:09:24 +0100

    Added url path for workshop-delete on workshops/urls.py

--- workshops/urls.py
+++ workshops/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import WorkshopListView, WorkshopDetailView, WorkshopCreateView, WorkshopUpdateView
+from .views import (WorkshopListView,
+                    WorkshopDetailView,
+                    WorkshopCreateView,
+                    WorkshopUpdateView,
+                    WorkshopDeleteView
+                    )
 from . import views
 
 urlpatterns = [
     path('', login_required(WorkshopListView.as_view()), name='workshops'),
-    path('<int:pk>/', login_required(WorkshopDetailView.as_view()), name='workshop-detail'),
+    path('<int:pk>/', login_required(WorkshopDetailView.as_view()),
+         name='workshop-detail'),
     path('new/', WorkshopCreateView.as_view(), name='workshop-create'),
-    path('<int:pk>/update', login_required(WorkshopUpdateView.as_view()), name='workshop-update'),
+    path('<int:pk>/update', WorkshopUpdateView.as_view(),
+         name='workshop-update'),
+    path('<int:pk>/delete', WorkshopDeleteView.as_view(),
+         name='workshop-delete'),
 ]
