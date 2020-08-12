ae3258ac5e4f0b9569e97dc5d88312bd09c51de9
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-12 19:33:32 +0100

    Updated url path name from adjust_bag to adjust-bag on bag/urls.py

--- bag/urls.py
+++ bag/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add-to-bag'),
-    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust-bag'),
     path('remove/<item_id>/', views.remove_from_bag, name='remove-from-bag,'),
 ]
