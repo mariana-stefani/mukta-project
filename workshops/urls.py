4941fcf2d2a36090d447dc6dee391a59fa15509c
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-12 18:37:42 +0100

    Added url path for workshop-update on workshops/urls.py

--- workshops/urls.py
+++ workshops/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import WorkshopListView, WorkshopDetailView, WorkshopCreateView
+from .views import WorkshopListView, WorkshopDetailView, WorkshopCreateView, WorkshopUpdateView
 from . import views
 
 urlpatterns = [
     path('', login_required(WorkshopListView.as_view()), name='workshops'),
     path('<int:pk>/', login_required(WorkshopDetailView.as_view()), name='workshop-detail'),
     path('new/', WorkshopCreateView.as_view(), name='workshop-create'),
+    path('<int:pk>/update', login_required(WorkshopUpdateView.as_view()), name='workshop-update'),
 ]
