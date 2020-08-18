20d06a61b6af655065ce9462d7a0076ee9a9936c
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-18 19:01:11 +0100

    Added order field on workshop model on workshops/models.py

--- workshops/models.py
+++ workshops/models.py
@@ -7,12 +7,13 @@ from datetime import datetime, date
 
 class Workshop(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField("Date*", auto_now_add=False, auto_now=False, blank=True)
+    date = models.DateField("Date YYYY-MM-DD*", auto_now_add=False, auto_now=False, blank=True)
     location = models.CharField(max_length=100)
     time = models.CharField(max_length=10)
     instructor = models.CharField(max_length=100)
     content = models.TextField()
     images = models.URLField(max_length=1024, null=True, blank=True)
+    order =  models.CharField("'order-first' or 'order-last'*", max_length=11, null=True, blank=True)
 
 
     def __str__(self):
