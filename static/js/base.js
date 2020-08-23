3b010f87c0f2c9bb75c267e00779d1e2a148b972
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-23 19:52:36 +0100

    Added js for back to top btn on base.js

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,22 @@
-// Toasts
+/**
+ * Toast initializer
+ */
+$(".toast").toast("show");
 
-$('.toast').toast('show');
+
+/**
+ * Back to top Button
+ */
+let topBtn = $("#top-btn");
+
+$("#top-btn").click(function () {
+  $(window).scrollTop(0);
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 0) {
+    topBtn.addClass("top-btn-show");
+  } else {
+    topBtn.removeClass("top-btn-show");
+  }
+});
