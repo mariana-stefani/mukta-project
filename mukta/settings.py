1b276f83c56ddd8fc2f2db6c1a197aedd1835231
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-26 19:58:21 +0100

    Updated debug mode on settings.py

--- mukta/settings.py
+++ mukta/settings.py
@@ -16,12 +16,7 @@ import django_heroku
 
 if os.path.exists("env.py"):
     import env
-
-if os.environ.get("DEV"):
-    development = True
-else:
-    development = False
-
+    
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['mukta-project.herokuapp.com', '127.0.0.1']
 
