5b955b5e96037d193bc594e13761462c93918020
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-26 16:23:05 +0100

    Added url path for product-delete on products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductCreateView, ProductUpdateView
+from .views import ProductCreateView, ProductUpdateView, ProductDeleteView
 from . import views
 
 
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product-detail'),
     path('new/', ProductCreateView.as_view(), name='product-create'),
-      path('<int:pk>/update',ProductUpdateView.as_view(),
+    path('<int:pk>/update',ProductUpdateView.as_view(),
          name='product-update'),
+    path('<int:pk>/delete', ProductDeleteView.as_view(),
+         name='product-delete'),
+    
 ]
