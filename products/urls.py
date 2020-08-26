78c16af23bad3b72eef70401e9cd4f0efd6ee117
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-26 16:04:28 +0100

    Added a url path for product-update on products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductCreateView
+from .views import ProductCreateView, ProductUpdateView
 from . import views
 
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product-detail'),
     path('new/', ProductCreateView.as_view(), name='product-create'),
+      path('<int:pk>/update',ProductUpdateView.as_view(),
+         name='product-update'),
 ]
