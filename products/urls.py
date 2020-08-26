b9557f129fb028ac0b0343ebd7e3289460d7cb1a
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-26 15:42:28 +0100

    Added url path for product-create on products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import ProductCreateView
 from . import views
+
 
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product-detail'),
+    path('new/', ProductCreateView.as_view(), name='product-create'),
 ]
