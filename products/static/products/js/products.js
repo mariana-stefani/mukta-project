1c1c3a020772cd8aa1e5d991a159a3938361ab9b
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 12:37:56 +0100

    Formated comment on products.js

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -1,22 +1,23 @@
-// Sort products selector
+/**
+ * Sort products selector
+ */
+$("#sort-selector").change(function () {
+  let selector = $(this);
+  let currentUrl = new URL(window.location);
 
-$('#sort-selector').change(function() {
-    let selector = $(this);
-    let currentUrl = new URL(window.location);
+  let selectedVal = selector.val();
+  if (selectedVal != "reset") {
+    let sort = selectedVal.split("_")[0];
+    let direction = selectedVal.split("_")[1];
 
-    let selectedVal = selector.val();
-    if(selectedVal != "reset"){
-        let sort = selectedVal.split("_")[0];
-        let direction = selectedVal.split("_")[1];
+    currentUrl.searchParams.set("sort", sort);
+    currentUrl.searchParams.set("direction", direction);
 
-        currentUrl.searchParams.set("sort", sort);
-        currentUrl.searchParams.set("direction", direction);
+    window.location.replace(currentUrl);
+  } else {
+    currentUrl.searchParams.delete("sort");
+    currentUrl.searchParams.delete("direction");
 
-        window.location.replace(currentUrl);
-    } else {
-        currentUrl.searchParams.delete("sort");
-        currentUrl.searchParams.delete("direction");
-
-        window.location.replace(currentUrl);
-    }
-})
+    window.location.replace(currentUrl);
+  }
+});
