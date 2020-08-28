3e33f0283fb12098bbae87fca94d9361756cc411
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 20:06:44 +0100

    Removed js to remove item from bag.js

--- bag/static/bag/js/bag.js
+++ bag/static/bag/js/bag.js
@@ -5,18 +5,3 @@ $(".update-link").click(function (e) {
   let form = $(this).prev(".update-form");
   form.submit();
 });
-
-/**
- * Remove item from bag and reload on click
- */
-$(".remove-item").click(function (e) {
-  let csrfToken = "{{ csrf_token }}";
-  let itemId = $(this).attr("id").split("remove_")[1];
-  let colour = $(this).data("product_colour");
-  let url = `/bag/remove/${itemId}/`;
-  let data = { 'csrfmiddlewaretoken': csrfToken, 'product_colour': colour };
-
-  $.post(url, data).done(function () {
-    location.reload();
-  });
-});
