be9dea56c9f8b8a9800be8c404c9400b3fee6431
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 12:27:59 +0100

    Removed comment Create your models here from workshops/models.py

--- workshops/models.py
+++ workshops/models.py
@@ -2,25 +2,24 @@ from django.db import models
 from django.urls import reverse
 from datetime import datetime, date
 
-# Create your models here.
-
 
 class Workshop(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField("Date (YYYY-MM-DD)*", auto_now_add=False, auto_now=False, blank=True)
+    date = models.DateField("Date (YYYY-MM-DD)*",
+                            auto_now_add=False, auto_now=False, blank=True)
     location = models.CharField(max_length=100)
     time = models.CharField(max_length=10)
     instructor = models.CharField(max_length=100)
     content = models.TextField()
-    images = models.URLField("Image URL*", max_length=1024, null=True, blank=True)
-    order =  models.CharField("Add 'order-md-first' or 'order-md-last' accordingly*", max_length=14, null=True, blank=True)
-    divider =  models.CharField("Add 'workshop-divider' (Unless it\'s the last workshop one on the page)*", max_length=16, null=True, blank=True)
-
+    images = models.URLField(
+        "Image URL*", max_length=1024, null=True, blank=True)
+    order = models.CharField(
+        "Add 'order-md-first' or 'order-md-last' accordingly*", max_length=14, null=True, blank=True)
+    divider = models.CharField(
+        "Add 'workshop-divider' (Unless it\'s the last workshop one on the page)*", max_length=16, null=True, blank=True)
 
     def __str__(self):
         return self.title
 
-    
     def get_absolute_url(self):
         return reverse('workshop-detail', kwargs={'pk': self.pk})
-
