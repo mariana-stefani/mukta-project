8b98e1b249de45e8cf57ec10b167dfe93a9c2349
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 16:24:02 +0100

    Added js to add class when user scroll down on base.js

--- static/js/base.js
+++ static/js/base.js
@@ -3,7 +3,6 @@
  */
 $(".toast").toast("show");
 
-
 /**
  * Back to top Button
  */
@@ -18,5 +17,17 @@ $(window).scroll(function () {
     topBtn.addClass("top-btn-show");
   } else {
     topBtn.removeClass("top-btn-show");
+  }
+});
+
+/**
+ * When user scroll an ID is added to the nav to add background-color
+ */
+$(window).scroll(function () {
+  let navbar = $(".navbar");
+  if ($(window).scrollTop() > 0) {
+    navbar.addClass("nav-show");
+  } else {
+    navbar.removeClass("nav-show");
   }
 });
