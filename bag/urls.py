2ab7122117833843253dd754114ded296fd81974
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 18:35:04 +0100

    Fixed typo on bag/urls.py

--- bag/urls.py
+++ bag/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.view_bag, name='view-bag'),
     path('add/<item_id>/', views.add_to_bag, name='add-to-bag'),
     path('adjust/<item_id>/', views.adjust_bag, name='adjust-bag'),
-    path('remove/<item_id>/', views.remove_from_bag, name='remove-from-bag,'),
+    path('remove/<item_id>/', views.remove_from_bag, name='remove-from-bag'),
 ]
