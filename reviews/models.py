3e3a94c1ca2e59721bf1b6dba619939ba972400d
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 12:27:34 +0100

    Removed comment Create your models here from reviews/models.py

--- reviews/models.py
+++ reviews/models.py
@@ -4,8 +4,6 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=10)
@@ -15,6 +13,5 @@ class Review(models.Model):
     def __str__(self):
         return self.title
 
-    
     def get_absolute_url(self):
         return reverse('review-detail', kwargs={'pk': self.pk})
