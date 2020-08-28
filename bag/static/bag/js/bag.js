7d1e552046a00d65a0d046c93462cf4c042e12de
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-28 12:02:22 +0100

    Formated comments on bag.js

--- bag/static/bag/js/bag.js
+++ bag/static/bag/js/bag.js
@@ -1,10 +1,14 @@
-// Updated bag quatity on click
+/**
+ * Updated bag quatity on click
+ */
 $(".update-link").click(function (e) {
   let form = $(this).prev(".update-form");
   form.submit();
 });
 
-// Remove item from bag and reload on click
+/**
+ * Remove item from bag and reload on click
+ */
 $(".remove-item").click(function (e) {
   let csrfToken = "{{ csrf_token }}";
   let itemId = $(this).attr("id").split("remove_")[1];
