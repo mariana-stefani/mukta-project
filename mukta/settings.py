f6202d7bc182cd41da1ef012c6d81201a5d01d9e
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-08-29 14:33:04 +0100

    Updated debug to true on settings.py

--- mukta/settings.py
+++ mukta/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mukta-project.herokuapp.com', '127.0.0.1']
 
